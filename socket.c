c07071c0d736a17f39e051d37f11b786c1151c38
DS0708 <jonum12312@gmail.com>
2024-09-30 09:18:36 +0900

    socket.c

--- socket.c
+++ socket.c
@@ -1,7 +1,16 @@
-int server_socket;
-server_socket = socket(PF_INET, SOCK_STREAM, 0);
-if(-1 == server_socket)
-{
-printf("server socket 생성 실패\n“);
-exit(1);
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main() {
+    int server_socket;
+    server_socket = socket(PF_INET, SOCK_STREAM, 0);
+
+    if (server_socket == -1) {
+        printf("서버 소켓 생성 실패\n");
+        exit(1);
+    }
+
+    return 0;
 }
