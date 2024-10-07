ec63961d091e5559e3c93ceb2c6498de3339c202
DS0708 <jonum12312@gmail.com>
2024-10-07 09:36:49 +0900

    client

--- clientSocket.c
+++ clientSocket.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv) {
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(4000);
+    server_addr.sin_port = htons(9000);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
