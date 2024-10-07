36e3bc6cece1284504441ed464a429fb8c2aaf14
DS0708 <jonum12312@gmail.com>
2024-10-07 09:34:26 +0900

    server

--- serverSocket.c
+++ serverSocket.c
@@ -25,9 +25,9 @@ int main(int argc, char **argv) {
     }
 
     // 서버 주소 설정
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(4000);
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_family = AF_INET; //Ipv4
+    server_addr.sin_port = htons(9000);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // 0.0.0.0/0 IP 허용
 
     // 바인드 에러 처리
     if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
@@ -35,37 +35,30 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    // 리슨 에러 처리
+    // 서버가 한 번에 최대 5개의 동시 연결 요청을 대기 큐에 보관할 수 있음을 의미
     if (listen(server_socket, 5) == -1) {
         perror("listen() 실행 실패");
         exit(1);
     }
 
     // 클라이언트 요청 처리 루프
-    while (1) {
-        client_addr_size = sizeof(client_addr);
-        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_size);
-        if (client_socket == -1) {
-            perror("클라이언트 연결 수락 실패");
-            continue;  // 실패시 다음 연결 대기
+    while (1) {  // 클라이언트와 지속적으로 데이터 송수신
+            ssize_t num_bytes_read = read(client_socket, buff_rcv, BUFF_SIZE);
+            if (num_bytes_read < 0) {
+                perror("read failed");
+                strcpy(buff_snd, "fail");
+                write(client_socket, buff_snd, strlen(buff_snd) + 1);
+            }else{
+                sprintf(buff_snd, "success : %s", buff_rcv);
+                write(client_socket, buff_snd, strlen(buff_snd) + 1);
+            }
+
+            printf("receive: %.*s\n", (int)num_bytes_read, buff_rcv);
+
+            if (strncmp(buff_rcv, "bye", 3) == 0) {
+                close(client_socket);
+            }
         }
 
-        // 클라이언트로부터 데이터 읽기
-        ssize_t num_bytes_read = read(client_socket, buff_rcv, BUFF_SIZE);
-        if (num_bytes_read < 0) {
-            perror("read failed");
-            close(client_socket);
-            continue;  // 읽기 실패 시 다음 연결 대기
-        }
-
-        printf("receive: %.*s\n", (int)num_bytes_read, buff_rcv);  // 안전하게 출력
-
-        // 클라이언트에게 응답 전송
-        sprintf(buff_snd, "%ld : %s", num_bytes_read, buff_rcv);
-        write(client_socket, buff_snd, strlen(buff_snd) + 1);  // NULL 포함 전송
-
-        close(client_socket);  // 클라이언트 소켓 닫기
-    }
-
-    return 0;  // 이 부분은 서버가 종료될 경우에 도달합니다.
+    return 0;  // 이 부분은 서버가 종료될 경우에 도달
 }
